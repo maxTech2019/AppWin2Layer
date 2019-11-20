efb6eb65b132364d2820d22b52d318d62a885f9f
Fredy Jordán Cordonez <fmaxjordan@gmail.com>
2019-11-20 09:29:13 -0500

    Punto Inicio Aplicación 2 capas.

---
+++ Layer2appWin/Layer2appWin/Negocio.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+public class Negocio
+{
+    public Negocio()
+    {
+        ArrayList Personas = new ArrayList();
+    }
+
+    public Persona CrearPersona(string pCodigo, string pNombres, string pApellidos, string pEmail, DateTime pFecha, decimal pSueldo)
+    {
+        Persona miPersona = new Persona();
+        miPersona.Codigo = pCodigo;
+        miPersona.Nombres = pNombres;
+        miPersona.Apellidos = pApellidos;
+        miPersona.Email = pEmail;
+        miPersona.FechNacimiento = pFecha;
+        miPersona.Sueldo = pSueldo;
+        Personas.Add(miPersona);
+    }
+
+}
---
+++ Layer2appWin/Layer2appWin/Persona.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+public class Persona
+{
+    public string Codigo { get; set; }
+    public string Nombres { get; set; }
+    public string Apellidos { get; set; }
+    public string Email { get; set; }
+    public DateTime FechNacimiento { get; set; }
+    public decimal Sueldo { get; set; }
+}
